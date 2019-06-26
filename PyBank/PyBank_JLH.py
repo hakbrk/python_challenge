808cd134742fe070f02e865d68e6f9dd2688cdf8
hakbrk <jhjhawkins@gmail.com>
2019-06-26 09:25:34 -0500

    Try to again get repo to push to git hub

--- PyBank/PyBank_JLH.py
+++ PyBank/PyBank_JLH.py
@@ -1,28 +1,36 @@
+#Import modules
 import os
 import csv
 
+#Define path to data file
 budget_csv = os.path.join('.','Resources', 'budget_data.csv')
 
+#Open data file and pass to csvreader
 with open(budget_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
 
+#Define lists to be utilized    
     profit_loss = []
     date = []
+#Populate lists with data from csv file   
     for row in csvreader:
         profit_loss.append(int(row[1]))
         date.append(row[0])
 
+#Calculate the change in profit / loss and write the result to list Change
 change = []
 for i in range(1, len(profit_loss)):
     change.append(profit_loss[i]-profit_loss[i-1])
 
+#Use variables to simplify outut
 max_profit = (max(change))
 min_profit = (min(change))
 max_profit_date = date[(change.index(max(change)))+1]
 min_profit_date = date[(change.index(min(change)))+1]
 
 
+#Create and write required output to text file
 with open("Financial Analysis.txt", "w") as f:
     print("Financial Analysis", file=f)
     print("----------------------------", file=f)
@@ -32,6 +40,8 @@ with open("Financial Analysis.txt", "w") as f:
     print(f'Greatest Increase in Profits: {max_profit_date} (${max_profit})', file=f)
     print(f'Greatest Decrease in Profits: {min_profit_date} (${min_profit})', file=f)
 
+
+#Print required data to terminal
 print("Financial Analysis")
 print("----------------------------")
 print(f'Total Months: {len(date)}')
---
+++ PyPoll/PyPoll_JLH.py
@@ -0,0 +1,61 @@
+#Import modules
+import os
+import csv
+
+#Define path to election_data.csv
+election_data = os.path.join('.','Resources', 'election_data.csv')
+
+#Open the csv file and pass to csvreader
+with open(election_data, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+
+#Create a Cadidate list for further data analysis    
+    candidate = []
+    for row in csvreader:
+        candidate.append(row[2])
+#Pass total_votes value to variable by using len of list candidate
+total_votes = (len(candidate))
+
+#Create a dictionary with unique candidate names and a count of how many occurances
+vote_count = {}
+for candidate in candidate:
+    count = vote_count.get(candidate, 0)
+    count += 1
+    vote_count[candidate] = count
+
+#Revised to the code below to automate the print output
+#for candidate, count in vote_count.items():
+    #print(candidate +': ' + str('{:.3f}'.format((count/total_votes)*100)) + '%' + " (" + str(count) + ')')
+#Not used after a more efficient print method was developed with a nested for loop
+    # Khan_vote = (vote_count['Khan'])
+    # Correy_vote = (vote_count['Correy'])
+    # Li_vote = (vote_count['Li'])
+    # OTooley_vote = (vote_count['O\'Tooley'])
+    # Khan_percent = ('{:.3f}'.format((Khan_vote/total_votes)*100))
+    # Correy_percent = ('{:.3f}'.format((Correy_vote/total_votes)*100))
+    # Li_percent = ('{:.3f}'.format((Li_vote/total_votes)*100))
+    # OTooley_percent = ('{:.3f}'.format((OTooley_vote/total_votes)*100))
+
+#Create a text file with the required output
+with open("Election_Results.txt", "w") as f:
+    print('Election Results', file=f)
+    print('---------------------------', file=f)
+    print(f'Total Votes: {total_votes}', file=f)
+    print('---------------------------', file=f)
+    for candidate, count in vote_count.items():
+        print(candidate +': ' + str('{:.3f}'.format((count/total_votes)*100)) + '%' + " (" + str(count) + ')', file=f)
+    print('---------------------------', file=f)
+    print(f'Winner: {max(vote_count, key=vote_count.get)}', file=f)
+    print('---------------------------', file=f)
+
+#Print required output to terminal
+print('Election Results')
+print('---------------------------')
+print(f'Total Votes: {total_votes}')
+print('---------------------------')
+for candidate, count in vote_count.items():
+    print(candidate +': ' + str('{:.3f}'.format((count/total_votes)*100)) + '%' + " (" + str(count) + ')')
+print('---------------------------')
+print(f'Winner: {max(vote_count, key=vote_count.get)}')
+print('---------------------------')
